1a17d52ff8ccce344c61a840b6ad515dcbf24bde
Kiran <ksha0044@student.monash.edu.au>
2023-10-01 01:17:09 +1000

    updated turning, initialised ekf covariance

--- Week08-09/auto_fruit_search.py
+++ Week08-09/auto_fruit_search.py
@@ -119,18 +119,33 @@ def drive_to_point(waypoint, robot_pose):
     wheel_vel = 30 # tick
     
     # turn towards the waypoint
-    turn_time = (baseline*2*np.pi)/(2*scale*wheel_vel) # replace with your calculation
+    full_turn_time = (baseline*2*np.pi)/(2*scale*wheel_vel) # time to do 1 rotation, so we need to rotate to get to the desired angle
+    
+    # we only need to do a partial turn. I
+    turn_time = full_turn_time*(robot_pose[2] - desired_angle)/(2*np.pi)
+    if turn_time < 0: # if the robot is turning the wrong way just swap the 1 and -1.
+        turn_direction = 1
+    else:
+        turn_direction = -1
+    turn_time = abs(turn_time)
     print("Turning for {:.2f} seconds".format(turn_time))
-    ppi.set_velocity([0, 1], turning_tick=wheel_vel, time=turn_time)
+    lv, rv = ppi.set_velocity([0, turn_direction], turning_tick=wheel_vel, time=turn_time)
+    drive_meas = measure.Drive(lv, rv, turn_time)
+    ekf.predict(drive_meas)
+
+
     
     # after turning, drive straight to the waypoint
     distance = np.sqrt((waypoint[0]-robot_pose[0])**2 + (waypoint[1]-robot_pose[1])**2)
     drive_time = distance/(scale*wheel_vel)
     print("Driving for {:.2f} seconds".format(drive_time))
-    ppi.set_velocity([1, 0], tick=wheel_vel, time=drive_time)
+    lv, rv = ppi.set_velocity([1, 0], tick=wheel_vel, time=drive_time)
+    drive_meas = measure.Drive(lv, rv, drive_time)
+    ekf.predict(drive_meas)
     ####################################################
 
     print("Arrived at [{}, {}]".format(waypoint[0], waypoint[1]))
+     
 
 
 def get_robot_pose():
@@ -142,23 +157,35 @@ def get_robot_pose():
     img = ppi.get_image()
 
     # detect the ArUco markers
-    lms, aruco_img = aruco.detect_marker_positions(img)
+    lms, aruco_img = aruco_det.detect_marker_positions(img)
 
     # update only the robots position
-    drive_meas = measure.Drive(lv, rv, dt)
-    ekf.predict(drive_meas)
+    # drive_meas = measure.Drive(lv, rv, dt)
+    # ekf.predict(drive_meas)
+    # drive_meas = measure.Drive(lv1, rv1, dt1)
+    # ekf.predict(drive_meas)
     ekf.add_landmarks(lms)
     ekf.update(lms)
 
 
     # update the robot pose [x,y,theta]
-    robot_pose = [0.0,0.0,0.0] # replace with your calculation
+    robot_pose = ekf.robot.state # replace with your calculation
     ####################################################
 
     return robot_pose
 
 # main loop
 if __name__ == "__main__":
+
+    # required in main.
+    # 1. pibot = PenguinPi(args.ip, args.port)
+    # 2. self.ekf = self.init_ekf(args.calib_dir, args.ip)
+    # self.aruco_det = aruco.aruco_detector(
+    #     self.ekf.robot, marker_length=0.07)  # size of the ARUCO markers
+
+    # 3. Maybe add yolo detector
+
+
     parser = argparse.ArgumentParser("Fruit searching")
     parser.add_argument("--map", type=str, default='M4_true_map_full.txt') # change to 'M4_true_map_part.txt' for lv2&3
     parser.add_argument("--ip", metavar='', type=str, default='192.168.50.1')
@@ -192,15 +219,28 @@ if __name__ == "__main__":
     baseline = np.loadtxt(fileB, delimiter=',')
     robot = Robot(baseline, scale, camera_matrix, dist_coeffs)
     ekf =  EKF(robot)
-    
+    aruco_det = aruco.aruco_detector(
+            ekf.robot, marker_length=0.07)
     ### ADDED CODE ###
 
     # read in the true map
     fruits_list, fruits_true_pos, aruco_true_pos = read_true_map(args.map)
     search_list = read_search_list()
     print_target_fruits_pos(search_list, fruits_list, fruits_true_pos)
+
+
+    # this may not be implemented correctly, maybe test my running ekf and printing self.markers to see what it is meant to look like
     ekf.markers = aruco_true_pos
+    
+    # will also have to verify that this is what it is meant to look like
     ekf.taglist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    # this for loop initialises the covariance of the 10 markers that we just added to be super low
+    for _ in range(len(ekf.taglist)):
+        ekf.P = np.concatenate((ekf.P, np.zeros((2, ekf.P.shape[1]))), axis=0)
+        ekf.P = np.concatenate((ekf.P, np.zeros((ekf.P.shape[0], 2))), axis=1)
+        ekf.P[-2,-2] = 0.1
+        ekf.P[-1,-1] = 0.1
 
     waypoint = [0.0,0.0]
     robot_pose = [0.0,0.0,0.0]
